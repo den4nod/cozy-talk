fd1e57eebc707449d47d3c91453aeee69abdfbaa
Denys Chechelnytskyy <den4nod@users.noreply.github.com>
2021-12-07 19:46:56 +0100

    Add post component (#1)
    * Initial draft of post component.
    * No styling applied.
    * Tags are concatenated in string with '#' prefix.
    * Future work includes: extracting subject, body and
    tags to separate components.
    
    Co-authored-by: Denys Chechelnytskyy <chechelnytskyy@corporater.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {PostContainer} from "./containers/post/post";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <PostContainer
+                    subject="First post"
+                    body="The first post in CozyTalk social network.
+                        The first post is very special.
+                        It is the face of each profile.
+                        It makes an impression on friends and
+                        other people in community who view your page."
+                    tags={["first post", "post", "cozy talk", "news"]}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
