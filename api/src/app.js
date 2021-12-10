c85a91bffec4541a0e8a925c5d40f63610214d58
Denys Chechelnytskyy <chechelnytskyy@corporater.com>
2021-12-10 01:44:18 +0100

    Configure eslint
    * Check syntax, find problems, and enforce code style.
    * Follow google style guide.

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('API test')
-})
+app.get('/', function(req, res) {
+  res.send('API test');
+});
 
-app.listen(process.env.APP_PORT)
+app.listen(process.env.APP_PORT);
