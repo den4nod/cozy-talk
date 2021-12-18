c3de3e5d6817ec71bb1c24290641593718802cea
Denys Chechelnytskyy <den4nod@users.noreply.github.com>
2021-12-18 10:13:33 +0200

    Configure eslint (#13)
    Co-authored-by: Denys Chechelnytskyy <chechelnytskyy@corporater.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import {PostContainer} from "./containers/post/post";
+import {PostContainer} from './containers/post/post';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <PostContainer
-                    subject="First post"
-                    body="The first post in CozyTalk social network.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PostContainer
+          subject="First post"
+          body="The first post in CozyTalk social network.
                         The first post is very special.
                         It is the face of each profile.
                         It makes an impression on friends and
                         other people in community who view your page."
-                    tags={["first post", "post", "cozy talk", "news"]}
-                />
-            </header>
-        </div>
-    );
+          tags={['first post', 'post', 'cozy talk', 'news']}
+        />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- front/src/containers/post/post.js
+++ front/src/containers/post/post.js
@@ -1,10 +1,10 @@
-import {Post} from "../../components/post/post";
+import {Post} from '../../components/post/post';
 
 export function PostContainer({subject, body, tags}) {
-    return <Post subject={subject} body={body} tags={hashtagString(tags)}/>
+  return <Post subject={subject} body={body} tags={hashtagString(tags)}/>
 }
 
 const hashtagString = (tags) => {
-    return tags.reduce((hashtagString, tag) =>
-        hashtagString + "#" + tag.split(" ").join("") + " ", "").trim();
+  return tags.reduce((hashtagString, tag) =>
+    hashtagString + '#' + tag.split(' ').join('') + ' ', '').trim();
 };
