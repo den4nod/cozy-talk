5765721649c6a63b20100e4a83d720f0061113d1
Denys Chechelnytskyy <den4nod@users.noreply.github.com>
2022-01-07 21:37:04 +0100

    Refactor code: (#19)
    * Extract page names and titles to constants.
    * Introduce a separate method for updating
    each page on click.
    
    Co-authored-by: Denys Chechelnytskyy <chechelnytskyy@corporater.com>

--- front/src/App.js
+++ front/src/App.js
@@ -3,11 +3,12 @@ import { HeaderContainer } from './containers/header/header'
 import { BodyContainer } from './containers/body/body'
 import { useState } from 'react'
 import Divider from '@mui/material/Divider'
+import { ARTICLES_PAGE, ARTICLES_PAGE_TITLE } from './constants'
 
 function App() {
 
-  const [page, setPage] = useState('articles')
-  const [pageTitle, setPageTitle] = useState('Articles')
+  const [page, setPage] = useState(ARTICLES_PAGE)
+  const [pageTitle, setPageTitle] = useState(ARTICLES_PAGE_TITLE)
 
   return (
     <div className='App'>
--- front/src/containers/body/body.js
+++ front/src/containers/body/body.js
@@ -1,13 +1,14 @@
 import { Articles } from '../../components/articles/articles'
 import { AddArticle } from '../../components/articles/addArticle'
 import { Profile } from '../../components/profile/profile'
+import { ADD_ARTICLE_PAGE, ARTICLES_PAGE, PROFILE_PAGE } from '../../constants'
 
 export function BodyContainer({ page, pageTitle }) {
   return (
     <div>
-      {page === 'articles' && <Articles pageTitle={pageTitle} />}
-      {page === 'addArticle' && <AddArticle pageTitle={pageTitle} />}
-      {page === 'profile' && <Profile pageTitle={pageTitle} />}
+      {page === ARTICLES_PAGE && <Articles pageTitle={pageTitle} />}
+      {page === ADD_ARTICLE_PAGE && <AddArticle pageTitle={pageTitle} />}
+      {page === PROFILE_PAGE && <Profile pageTitle={pageTitle} />}
     </div>
   )
 }
