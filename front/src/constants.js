573967b7da22b9fe7cd7b0277d3d1310d4b4ff27
Denys Chechelnytskyy <chechelnytskyy@corporater.com>
2022-01-07 15:24:06 +0100

    Refactor code: * Extract page names and titles to constants. * Introduce a separate method for updating each page on click.

---
+++ front/src/constants.js
@@ -0,0 +1,6 @@
+export const ARTICLES_PAGE = 'articles'
+export const ARTICLES_PAGE_TITLE = 'Articles'
+export const ADD_ARTICLE_PAGE = 'addArticle'
+export const ADD_ARTICLE_PAGE_TITLE = 'Add article'
+export const PROFILE_PAGE = 'profile'
+export const PROFILE_PAGE_TITLE = 'Profile'
--- front/src/containers/header/header.js
+++ front/src/containers/header/header.js
@@ -1,27 +1,44 @@
 import Button from '@mui/material/Button'
 import { ButtonGroup } from '@mui/material'
+import {
+  ADD_ARTICLE_PAGE,
+  ADD_ARTICLE_PAGE_TITLE,
+  ARTICLES_PAGE,
+  ARTICLES_PAGE_TITLE,
+  PROFILE_PAGE, PROFILE_PAGE_TITLE
+} from '../../constants'
+
+const VARIANT_CONTAINED = 'contained'
+const VARIANT_TEXT = 'text'
 
 export function HeaderContainer({ page, onPageChange, onPageTitleChange }) {
 
-  const updatePage = (page, pageTitle) => {
-    onPageChange(page)
-    onPageTitleChange(pageTitle)
+  const updateArticlesPage = () => {
+    onPageChange(ARTICLES_PAGE)
+    onPageTitleChange(ARTICLES_PAGE_TITLE)
+  }
+
+  const updateAddArticlePage = () => {
+    onPageChange(ADD_ARTICLE_PAGE)
+    onPageTitleChange(ADD_ARTICLE_PAGE_TITLE)
+  }
+
+  const updateProfilePage = () => {
+    onPageChange(PROFILE_PAGE)
+    onPageTitleChange(PROFILE_PAGE_TITLE)
   }
 
   return (
     <div>
-      <ButtonGroup variant='text' aria-label='text button group'>
-        <Button variant={page === 'articles' ? 'contained' : 'text'}
-          onClick={() => updatePage('articles', 'Articles')}>
-          Articles
+      <ButtonGroup variant={VARIANT_TEXT} aria-label='text button group'>
+        <Button variant={page === ARTICLES_PAGE ? VARIANT_CONTAINED : VARIANT_TEXT} onClick={updateArticlesPage}>
+          {ARTICLES_PAGE_TITLE}
         </Button>
-        <Button variant={page === 'addArticle' ? 'contained' : 'text'}
-          onClick={() => updatePage('addArticle', 'Add article')}>
-          Add article
+        <Button variant={page === ADD_ARTICLE_PAGE ? VARIANT_CONTAINED : VARIANT_TEXT} onClick={updateAddArticlePage}>
+          {ADD_ARTICLE_PAGE_TITLE}
         </Button>
-        <Button variant={page === 'profile' ? 'contained' : 'text'}
-          onClick={() => updatePage('profile', 'Profile')}>
-          Profile
+        <Button variant={page === PROFILE_PAGE ? VARIANT_CONTAINED : VARIANT_TEXT} onClick={updateProfilePage}>
+          {PROFILE_PAGE_TITLE}
         </Button>
       </ButtonGroup>
     </div>
