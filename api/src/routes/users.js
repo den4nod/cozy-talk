ec3d6660b47decc39c989bb449080473d83fca67
Denys Chechelnytskyy <den4nod@users.noreply.github.com>
2022-01-09 19:35:54 +0100

    Refactor users api (#23)
    Extract some constants
    
    Co-authored-by: Denys Chechelnytskyy <chechelnytskyy@corporater.com>

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -5,61 +5,84 @@ const bodyParser = require('body-parser')
 const router = express.Router()
 const jsonParser = bodyParser.json()
 
+const USERS_TABLE = 'users'
+const SUCCESS_JSON_RESPONSE = { status: 'success' }
+const BAD_REQUEST_STATUS_CODE = 400
+
+const errorJsonResponse = (error) => {
+  return { error: error }
+}
+
 router.get('/', async function (req, res) {
-  res.json(await db('users').orderBy('user_id'))
+  res.json(await db(USERS_TABLE).orderBy('user_id'))
 })
 
 router.post('/', jsonParser, async function (req, res) {
-  console.log(req.body)
   const { name, email, phone } = req.body
   if (!name) {
-    return res.status(400).json({ error: 'Name cannot be empty' })
+    return res
+      .status(BAD_REQUEST_STATUS_CODE)
+      .json(errorJsonResponse('Name cannot be empty'))
   }
   if (!email) {
-    return res.status(400).json({ error: 'Email cannot be empty' })
+    return res
+      .status(BAD_REQUEST_STATUS_CODE)
+      .json(errorJsonResponse('Email cannot be empty'))
   }
   if (!phone) {
-    return res.status(400).json({ error: 'Phone cannot be empty' })
+    return res
+      .status(BAD_REQUEST_STATUS_CODE)
+      .json(errorJsonResponse('Phone cannot be empty'))
   }
-  await db('users')
+  await db(USERS_TABLE)
     .insert({ name: name, email: email, phone: phone })
-    .then(() => res.json({ status: 'success' }))
+    .then(() => res.json(SUCCESS_JSON_RESPONSE))
     .catch((error) => {
-      return res.status(400).json({ error: error.name })
+      return res
+        .status(BAD_REQUEST_STATUS_CODE)
+        .json(errorJsonResponse(error.name))
     })
 })
 
 router.get('/:id', async function (req, res) {
   const { id } = req.params
-  res.json(await db('users').where({ user_id: id }))
+  res.json(await db(USERS_TABLE).where({ user_id: id }))
 })
 
 router.put('/:id', jsonParser, async function (req, res) {
   const { id } = req.params
   if (!id) {
-    return res.status(400).json({ error: 'User id cannot be empty' })
+    return res
+      .status(BAD_REQUEST_STATUS_CODE)
+      .json(errorJsonResponse('User ID cannot be empty'))
   }
   const { name, email, phone } = req.body
-  await db('users')
+  await db(USERS_TABLE)
     .where({ user_id: id })
     .update({ name: name, email: email, phone: phone })
-    .then(() => res.json({ status: 'success' }))
+    .then(() => res.json(SUCCESS_JSON_RESPONSE))
     .catch((error) => {
-      return res.status(400).json({ error: error.name })
+      return res
+        .status(BAD_REQUEST_STATUS_CODE)
+        .json(errorJsonResponse(error.name))
     })
 })
 
 router.delete('/:id', function (req, res) {
   const { id } = req.params
   if (!id) {
-    return res.status(400).json({ error: 'User id cannot be empty' })
+    return res
+      .status(BAD_REQUEST_STATUS_CODE)
+      .json(errorJsonResponse('User ID cannot be empty'))
   }
-  db('users')
+  db(USERS_TABLE)
     .where({ user_id: id })
     .del()
-    .then(() => res.json({ status: 'success' }))
+    .then(() => res.json(SUCCESS_JSON_RESPONSE))
     .catch((error) => {
-      return res.status(400).json({ error: error.name })
+      return res
+        .status(BAD_REQUEST_STATUS_CODE)
+        .json(errorJsonResponse(error.name))
     })
 })
 
