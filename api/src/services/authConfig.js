e154400dcbbd974a77033008b3bafa0b046ed145
Denys Chechelnytskyy <chechelnytskyy@corporater.com>
2022-02-21 21:40:15 +0100

    Add fb config

--- api/src/services/authConfig.js
+++ api/src/services/authConfig.js
@@ -2,5 +2,9 @@ module.exports = {
   googleAuth: {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
+  },
+  fbAuth: {
+    appId: process.env.FB_APP_ID,
+    appSecret: process.env.FB_APP_SECRET
   }
 }
