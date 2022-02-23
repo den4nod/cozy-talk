422aa0cc1084c4151fb88e992be2c4f07ffbf8cb
Denys Chechelnytskyy <chechelnytskyy@corporater.com>
2022-02-23 15:49:56 +0100

    Add authentication context

--- front/src/components/profile/userProfile.js
+++ front/src/components/profile/userProfile.js
@@ -1,6 +1,15 @@
 import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import authContext from '../../contexts/authContext'
 
 export function UserProfile({ fullName, age }) {
+
+  const { authenticated, accessToken, expiresIn } = useContext(authContext)
+  // for task, todo: remove later
+  console.log('authenticated: ', authenticated)
+  console.log('accessToken: ', accessToken)
+  console.log('expiresIn: ', expiresIn)
+
   return (
     <div>
       <p><b>Name:</b> {fullName}</p>
